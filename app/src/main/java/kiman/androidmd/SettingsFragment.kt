dedd583eda6df36f1d9903c63505d89229294259
leivoev <leivoev@gmail.com>
2020-08-04 21:54:58 +0900

    Update SettingsFragment.kt
    Make Shared Preference Listener

--- app/src/main/java/kiman/androidmd/SettingsFragment.kt
+++ app/src/main/java/kiman/androidmd/SettingsFragment.kt
@@ -1,13 +1,29 @@
 package kiman.androidmd
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.preference.PreferenceFragmentCompat
 
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(prefListener)
+    }
+
+    var prefListener:SharedPreferences.OnSharedPreferenceChangeListener =
+        object : SharedPreferences.OnSharedPreferenceChangeListener {
+
+        override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+            if (key == "switch_background") {
+                Log.d("PrefLog", "switch_background is changed")
+            }
+
+            else if (key == "switch_limit") {
+                Log.d("PrefLog", "switch_limit is changed")
+            }
+        }
     }
 }
