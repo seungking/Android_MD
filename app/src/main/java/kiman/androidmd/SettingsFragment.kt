b389023016f4c97f5465c996bba0238b001ba95b
leivoev <leivoev@gmail.com>
2020-08-06 18:12:48 +0900

    Add Alert Popup in Setting Fragment
    When the switch is turned on, a popup appears.

--- app/src/main/java/kiman/androidmd/SettingsFragment.kt
+++ app/src/main/java/kiman/androidmd/SettingsFragment.kt
@@ -1,8 +1,11 @@
 package kiman.androidmd
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.preference.PreferenceFragmentCompat
 
 
@@ -21,17 +24,27 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 Log.d("PrefLog", "switch_background is changed")
                 Log.d("PrefLog", sharedPreferences.getBoolean("switch_background", true).toString())
                 if (sharedPreferences.getBoolean("switch_background", true)) {
-                    showAlertPopup()
+                    val string_back:String = "백그라운드에서의 실행을 허용합니다."
+                    showAlertPopup(string_back)
                 }
             }
 
             else if (key == "switch_limit") {
                 Log.d("PrefLog", "switch_limit is changed")
+                if (sharedPreferences.getBoolean("switch_limit", true)) {
+                    val string_limit:String = "실행 가능한 동작의 수를 5개로 제한합니다." + "해제시 인식률이 떨어집니다."
+                    showAlertPopup(string_limit)
+                }
             }
         }
     }
 
-    private fun showAlertPopup() {
-        TODO("Not yet implemented")
+    private fun showAlertPopup(string: String) {
+        val alertDialog = AlertDialog.Builder(this.activity)
+            .setTitle(string)
+            .setPositiveButton("OK", null)
+            .create()
+
+        alertDialog.show()
     }
 }
